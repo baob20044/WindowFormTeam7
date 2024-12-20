fe8492761d02d0f8836cbe8d46ff779b91c67882
Thanh Danh <thanhdanh431@gmail.com>
2024-12-20 11:21:44 +0700

    add load profile employee

--- StoreManage/DTOs/Employee/EmployeeUpdateDto.cs
+++ StoreManage/DTOs/Employee/EmployeeUpdateDto.cs
@@ -9,14 +9,10 @@ namespace StoreManage.DTOs.Employee
         public EmployeePersonalInfo PersonalInfo { get; set; }
         public decimal Salary { get; set; }
 
-        [Required(ErrorMessage = "Start date is required.")]
-        [DataType(DataType.Date, ErrorMessage = "Invalid date format (yyyy-MM-dd)")]
-        public DateTime StartDate { get; set; }
+        public string StartDate { get; set; }
 
-        [Required(ErrorMessage = "ContractUpTo is required.")]
         public int ContractUpTo { get; set; }
 
-        [Phone(ErrorMessage = "Please enter a valid phone number.")]
         public string ParentPhoneNumber { get; set; }
     }
 }
